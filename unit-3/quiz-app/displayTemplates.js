dd59e87e2d7f6402bc9df5ed8101b864e79a90f1
unknown <clayton.weller@gmail.com>
2018-06-19 00:38:29 -0600

    Got the basic flow of the app working, with wire-frames

---
+++ unit-3/quiz-app/displayTemplates.js
@@ -0,0 +1,14 @@
+const displayTemplates = {
+    start: function(){
+        return `
+            <div class="tv">
+            </div> 
+            <div class="interface">
+                <button type="submit" class="js-continue-button continue">Lets Find a Series!</button>
+            </div>
+            <div class="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus culpa deserunt, tenetur eum velit aliquid sit consectetur commodi debitis atque at nulla quam aut dolor architecto molestiae ut adipisci assumenda?
+            </div>
+        `
+    }
+}
