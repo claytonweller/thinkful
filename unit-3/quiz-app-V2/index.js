921904046d43aed2b257c11c1f18bc9e34c76aba
unknown <clayton.weller@gmail.com>
2018-06-26 13:17:26 -0600

    Refactor to make STATE smaller

--- unit-3/quiz-app-V2/index.js
+++ unit-3/quiz-app-V2/index.js
@@ -1,212 +1,151 @@
-const STORE = {
+const STATE = {
     currentQuestion:0,
-    totalCorrect:0,
-    currentDisplay:'starting',
-    previousResponse:'PREVIOUS RESPONSE',
-    readyToProgress:true,
-}
-
-const questionList = [
+    totalCorrect:0
+  }
+  
+  const questionList = [
     {
-        question: 'How are you feeling?',
-        answers:['Good', 'Great', 'Super Good', 'Really Great'],
-        correct:'Super Good',
+      question: 'How are you feeling?',
+      answers:['Good', 'Great', 'Super Good', 'Really Great'],
+      correct:'Super Good',
+    },
+  
+    {
+      question: 'How were you feeling yesterday?',
+      answers:['Bad', 'Terrible', 'Crummy', 'Awful'],
+      correct:'Bad',
     },
     {
-        question: 'How were you feeling yesterday?',
-        answers:['Bad', 'Terrible', 'Crummy', 'Awful'],
-        correct:'Bad',
+      question: 'Does this seem arbitrary?',
+      answers:['Yes', 'Definitely', 'Sure Does', 'Pineapple'],
+      correct:'Pineapple',
     },
     {
-        question: 'Does this seem arbitrary?',
-        answers:['Yes', 'Definitely', 'Sure Does', 'Pineapple'],
-        correct:'Pineapple',
+      question: 'Who is the best character in starwars?',
+      answers:['Luke', 'The grandpa guy', 'The green grandpa guy', 'Black Bucket Hat Man'],
+      correct:'OBVIOUSLY HAN SOLO!',
     },
     {
-        question: 'Who is the best character in starwars?',
-        answers:['Luke', 'The grandpa guy', 'The green grandpa guy', 'Black Bucket Hat Man'],
-        correct:'OBVIOUSLY HAN SOLO!',
-    },
-    {
-        question: 'Can you get this question right?',
-        answers:['No', 'No', 'No', 'No'],
-        correct:'No',
+      question: 'Can you get this question right?',
+      answers:['No', 'No', 'No', 'No'],
+      correct:'No',
     }
-]
-
-const startClick = ()=>{
+  ];
+  
+  
+  // LISTENERS
+  
+  const setupListeners = ()=>{
+    $('#start').click(startClick);
+    $('#asking-form').submit(questionFormSubmit);
+    $('#feedbacking-button').click(feedbackClick);
+    $('#restart').click(restartClick);
+  }
+  
+  // HANDLERS
+  
+  const startClick = ()=>{
     $('.starting').attr('hidden', true)
-    renderAsking()
+    renderQuestion()
     $('.asking').attr('hidden', false)
-    STORE.currentDisplay = 'asking'
-    STORE.readyToProgress = false
-}
-
-const askClick = ()=>{
+  }
+  
+  const questionFormSubmit = event =>{
+    event.preventDefault()
     $('.asking').attr('hidden', true)
-    renderFeedbacking()
+    let answerIndex = $('input[name=possible-answer]:checked').val()
+    let answer = questionList[STATE.currentQuestion].answers[answerIndex];
+    renderFeedbacking(answer)
     $('.feedbacking').attr('hidden', false)
-    STORE.currentDisplay = 'feedbacking'
-    STORE.currentQuestion++
-}
-
-const feedbackClick = ()=>{
-    if(STORE.currentQuestion > questionList.length-1){
-        console.log('judging')
-        $('.feedbacking').attr('hidden', true)
-        renderJudging()
-        $('.judging').attr('hidden', false)
-        STORE.currentDisplay = 'judging'
+    STATE.currentQuestion++
+  }
+  
+  const feedbackClick = ()=>{
+    if(STATE.currentQuestion > questionList.length-1){
+      console.log('judging')
+      $('.feedbacking').attr('hidden', true)
+      renderJudging()
+      $('.judging').attr('hidden', false)
     } else {
-        $('.feedbacking').attr('hidden', true)
-        renderAsking()
-        $('.asking').attr('hidden', false)
-        STORE.currentDisplay = 'asking'
-        STORE.readyToProgress = false
+      $('.feedbacking').attr('hidden', true)
+      renderQuestion()
+      $('.asking').attr('hidden', false)
     }
-} 
-
-const restartClick = () =>{
+  }
+  
+  const restartClick = () =>{
     $('.judging').attr('hidden', true)
     $('.starting').attr('hidden', false)
-    STORE.currentDisplay = 'starting'
-    STORE.currentQuestion = 0
-    STORE.totalCorrect = 0
-}
-
-const continueClick = ()=>{
-    console.log('continueClick', STORE.currentDisplay)
-    if(STORE.readyToProgress){
-        if(STORE.currentDisplay === 'starting'){
-            startClick()
-        } else if (STORE.currentDisplay === 'asking') {
-            askClick()
-        } else if (STORE.currentDisplay === 'feedbacking'){
-            feedbackClick()
-        } else if (STORE.currentDisplay === 'judging'){
-            restartClick()
-        }    
-    }
-}
-
-const listenForContinueClick = ()=>{
-    console.log('listening for continue click')
-    $('.container').on('click', 'button', function(event){
-        event.preventDefault()
-        continueClick()
-    })
-}
-
-const radioClick = (answer)=>{
-    STORE.previousResponse = answer
-    STORE.readyToProgress = true
-}
-
-const listenForRadioClick = ()=>{
-    console.log('listening for Radio clicks')
-    $('.js-answers').on('click', 'li', function(event){
-        let answer = $(this).text()
-        radioClick(answer)
-    })
-}
-
-////renders the judging section
-
-const renderJudging = ()=>{
-    $('.judging').find('h1').html(`You got ${STORE.totalCorrect} correct out of ${questionList.length}`)
-}
-
-////renders the FEEDBACKING section
-
-const renderButtonText = ()=>{
-    if(STORE.currentQuestion === questionList.length-1){
-        $('.feedbacking').find('button').html(
-            'Is it over yet!?!'
-        )
+    STATE.currentQuestion = 0
+    STATE.totalCorrect = 0
+  }
+  
+  // QUESTION RENDERERS
+  
+  const renderJudging = ()=>{
+    $('.judging').find('h1').html(`You got ${STATE.totalCorrect} correct out of ${questionList.length}`)
+  }
+  
+  const renderButtonText = ()=>{
+    if(STATE.currentQuestion === questionList.length-1){
+      $('.feedbacking').find('button').html(
+        'Is it over yet!?!'
+      )
     }else{
-        $('.feedbacking').find('button').html(
-            'Lets move on...'
-        )
+      $('.feedbacking').find('button').html(
+        'Lets move on...'
+      )
     }
-
-}
-
-const renderSelection = () =>{
-    $('.feedbacking').find('h1').html(
-        `You picked - ${STORE.previousResponse}`
-    )
-}
-
-const theyWereRight = (guess) =>{
-    guess = guess.trim()
-    console.log(questionList[STORE.currentQuestion].correct)
-    if(guess === questionList[STORE.currentQuestion].correct){
-        STORE.totalCorrect++
-        return true
+  }
+  
+  const renderSelection = (answer) =>{
+    $('.feedbacking').find('h1').html( `You picked - ${answer}` )
+  }
+  
+  const renderValidation = answer =>{
+    let correct = questionList[STATE.currentQuestion].correct
+    let feedback = ""
+    if(guess === correct){
+      STATE.totalCorrect++
+      feedback = "You're right! NICE!"
     } else {
-        return false
+      feedback = `Sorry the correct answer was ${correct}`
     }
-}
-
-const renderValidation = () =>{
-    if(theyWereRight(STORE.previousResponse)){
-        $('.feedbacking').find('h2').html(
-            `You're right! NICE!`
-        )
-    } else {
-        $('.feedbacking').find('h2').html(
-            'Sorry the correct answer was '+questionList[STORE.currentQuestion].correct 
-        )
-    }
-    
-
-}
-
-const renderStatus = () =>{
+    $('.feedbacking').find('h2').html(feedback)
+  }
+  
+  const renderStatus = () =>{
     $('.feedbacking').find('h3').html(
-        `You have gotten ${STORE.totalCorrect} correct out of ${STORE.currentQuestion+1}`
+      `You have gotten ${STATE.totalCorrect} correct out of ${STATE.currentQuestion+1}`
     )
-}
-
-const renderFeedbacking = ()=>{
-    renderSelection()
-    renderValidation()
+  }
+  
+  const renderFeedbacking = answer =>{
+    renderSelection(answer)
+    renderValidation(answer)
     renderStatus()
     renderButtonText()
-}
-
-///renders the ASKING section
-
-const renderQuestion = () => {
-    $('.asking').find('h1').html(questionList[STORE.currentQuestion].question)
-}
-const renderSingleAnswer = (answer) => {
-    return `<li class="answer-container">
-                <label><input type="radio" class="js-possible-answer" name="possible-answer" value=${answer} id=${answer}/>${answer}</label>
-             </li>`
-}
-const renderProgress = () =>{
+  }
+  
+  // ASKING RENDERERS
+  
+  const renderSingleAnswer = (answer, i) => {
+    return `
+                <label class="answer-container">
+                <input type="radio" class="js-possible-answer" name="possible-answer" value=${i} id=${answer} required/>
+                ${answer}
+                </label>
+            `
+  }
+  
+  const renderQuestion = ()=>{
+    $('.asking').find('h1').html(questionList[STATE.currentQuestion].question);
     $('.asking').find('p').html(
-        `You are on question ${STORE.currentQuestion+1} of ${questionList.length}`
+      `You are on question ${STATE.currentQuestion+1} of ${questionList.length}`
     )
-}
-const renderAllAnswers = () =>{
     $('.asking').find('ul').html(
-        questionList[STORE.currentQuestion].answers.map(answer => renderSingleAnswer(answer))
+      questionList[STATE.currentQuestion].answers.map((answer, i) => renderSingleAnswer(answer, i))
     )
-}
-
-const renderAsking = ()=>{
-    renderQuestion()
-    renderAllAnswers()
-    renderProgress()
-}
-
-///activates all the listeners in the entire app
-
-function handleQuizApp() {
-    listenForContinueClick()
-    listenForRadioClick()
-}
-
-$(handleQuizApp);
+  }
+  
+  $(setupListeners);
