589d4959ea5afcdbfe52016a9ba3488a63657521
Sebastian Bastidas <sbastidasr@gmail.com>
2018-07-10 13:32:44 -0500

    giphy

--- S2-async/perfect-tweet/api-calls/getGiphyFromSearch.js
+++ S2-async/perfect-tweet/api-calls/getGiphyFromSearch.js
@@ -1,22 +1,28 @@
-const GIF_SEARCH_URL = 'http://api.giphy.com/v1/gifs/search';
-const GIPHY_API_KEY = 'lkdzgGGUC5PbHv3W33MJbdvhSMA1u4Qc'
-
-const createGiphyObject = (results) => {
-  let output = results.data.map(item => {
-    return {
-      url: item.images.original.url,
-      alt: item.title,
-    }
-  })
-  STATE.info.giphy = output
-  populateGiphy()
-}
+const GIF_SEARCH_URL = "http://api.giphy.com/v1/gifs/search";
+const GIPHY_API_KEY = "lkdzgGGUC5PbHv3W33MJbdvhSMA1u4Qc";
 
 function getGiphyFromSearch(search) {
   const query = {
     q: search,
-    'api_key': GIPHY_API_KEY,
-    limit: 4,
-  }
-  $.getJSON(GIF_SEARCH_URL, query, createGiphyObject);
+    api_key: GIPHY_API_KEY,
+    limit: 4
+  };
+  $.getJSON(GIF_SEARCH_URL, query, populateGiphy);
 }
+
+const populateGiphy = results => {
+  // $(".giphy-header")
+  //   .find("h1")
+  //   .html("Gifs related to: " + STATE.topic);
+  let output = results.data.map(renderGiphyObject);
+  $(".giphy-gifs").html(output);
+};
+
+const renderGiphyObject = item => `
+    <div class="gif-block">
+        <img src="${item.images.original.url}" alt="${item.title}" />
+    </div>`;
+
+// No state
+// Map once
+// One array
--- S2-async/perfect-tweet/index.js
+++ S2-async/perfect-tweet/index.js
@@ -1,57 +1,82 @@
+STATE = {
+  topic: "",
+  info: {
+    wiki: {
+      title: "",
+      extract: ""
+    },
+    // giphy: [],
+    news: {
+      good: [],
+      everything: []
+    },
+    twitter: []
+  }
+};
+
+const searchButtonClick = () => {
+  let topicField = $(".start-screen").find("input");
+  STATE.topic = $(topicField).val();
+  if (!STATE.topic) {
+    STATE.topic = "NOTHING";
+  }
+  $(topicField).val("");
+  switchToPerfectTweetScreen();
+  makeAPIcalls(STATE.topic);
+};
+
+const makeAPIcalls = topic => {
+  getGiphyFromSearch(topic);
+
+  getWikiFromSearch(topic);
+
+  getNewsFromSearch(topic);
+  getTwitterFromSearch(topic);
+};
+
+/////////////////
+
 const populateWiki = () => {
-  $('.wiki-title').find('h1').html(STATE.info.wiki.title)
-  $('.wiki-text').find('p').html(STATE.info.wiki.extract)
-}
+  $(".wiki-title")
+    .find("h1")
+    .html(STATE.info.wiki.title);
+  $(".wiki-text")
+    .find("p")
+    .html(STATE.info.wiki.extract);
+};
 
-const createGifBlocks = () => {
-  let html = ''
-  STATE.info.giphy.forEach(obj => {
-    html = html +
-      `<div class="gif-block">
-          <img src="${obj.url}" alt="${obj.alt}" />
-      </div>`
-  })
-  return html
-}
-
-const populateGiphy = () => {
-  $('.giphy-header').find('h1').html('Gifs related to: ' + STATE.topic)
-  $('.giphy-gifs').html(createGifBlocks())
-}
-
-const createSingleNewsArticle = (obj) => {
+const createSingleNewsArticle = obj => {
   return `
     <article class="news-article">
       <header class="news-headline">
-        <a href="${obj.url}"><h2>${obj.headline}</h2></a> 
+        <a href="${obj.url}"><h2>${obj.headline}</h2></a>
         <h3>${obj.source} - by ${obj.author}</h3>
       </header>
       <div class="news-text">
         <p>${obj.excerpt}</p>
       </div>
     </article>
-    `
-}
+    `;
+};
 
 const createAllNewsArticles = () => {
-
-  let articleHtml = '';
+  let articleHtml = "";
   STATE.info.news.good.forEach(item => {
-    articleHtml = articleHtml + createSingleNewsArticle(item)
-  })
+    articleHtml = articleHtml + createSingleNewsArticle(item);
+  });
   STATE.info.news.everything.forEach(item => {
-    articleHtml = articleHtml + createSingleNewsArticle(item)
-  })
-  return articleHtml
-}
+    articleHtml = articleHtml + createSingleNewsArticle(item);
+  });
+  return articleHtml;
+};
 
 const populateNews = () => {
-  $('.all-articles').html(createAllNewsArticles())
-  $('.js-topic').html(STATE.topic)
-}
+  $(".all-articles").html(createAllNewsArticles());
+  $(".js-topic").html(STATE.topic);
+};
 
-const createTwitterUser = (obj) =>{
-  let scaledUrl = obj.imageURL.replace('_normal', '_200x200')
+const createTwitterUser = obj => {
+  let scaledUrl = obj.imageURL.replace("_normal", "_200x200");
   return `
     <div class="twitter-user">
       <div class="twitter-user-splash">
@@ -72,124 +97,101 @@ const createTwitterUser = (obj) =>{
         <p>${obj.text}</p>
       </div>
     </div>
-  `
-}
+  `;
+};
 
-const createAllTwitterUsers = () =>{
-  let twitterHtml='';
-  let userCount =3
-  if(STATE.info.twitter.length <3 ){
-    userCount = STATE.info.twitter.length
+const createAllTwitterUsers = () => {
+  let twitterHtml = "";
+  let userCount = 3;
+  if (STATE.info.twitter.length < 3) {
+    userCount = STATE.info.twitter.length;
   }
   for (let index = 0; index < userCount; index++) {
-    twitterHtml += createTwitterUser(STATE.info.twitter[index]) 
+    twitterHtml += createTwitterUser(STATE.info.twitter[index]);
   }
 
-  return twitterHtml
-}
+  return twitterHtml;
+};
 
-
-const populateTwitter = () =>{
-  console.log('populateTwitter')
-  $('.twitter-users').html(createAllTwitterUsers())
-}
+const populateTwitter = () => {
+  console.log("populateTwitter");
+  $(".twitter-users").html(createAllTwitterUsers());
+};
 
 const populateInfo = () => {
-  populateWiki()
-  populateGiphy()
-  populateNews()
-  populateTwitter()
-}
+  populateWiki();
+  populateGiphy();
+  populateNews();
+  populateTwitter();
+};
 
-const makeAPIcalls = (topic) => {
-  getWikiFromSearch(topic)
-  getGiphyFromSearch(topic)
-  getNewsFromSearch(topic)
-  getTwitterFromSearch(topic)
-}
-
-const truncateLongSearchString = (string)=>{
-  let smallTopicArray = string.split(' ')
-  if (smallTopicArray.length > 2){
-    return smallTopicArray.sort((a, b) => b.length - a.length )[0]
+const truncateLongSearchString = string => {
+  let smallTopicArray = string.split(" ");
+  if (smallTopicArray.length > 2) {
+    return smallTopicArray.sort((a, b) => b.length - a.length)[0];
   } else {
-    return string
+    return string;
   }
-}
+};
 
 const switchToPerfectTweetScreen = () => {
-  STATE.currentScreen = 'perfect-tweet'
-  $('.start-screen').attr('hidden', true)
-  $('.perfect-tweet-screen').attr('hidden', false)
-}
-
-const searchButtonClick = () => {
-  let topicField = $('.start-screen').find('input')
-  let topic = $(topicField).val()
-  if (topic === '') {
-    topic = 'NOTHING'
-  }
-  STATE.topic = topic
-  $(topicField).val('')
-  switchToPerfectTweetScreen()
-  makeAPIcalls(topic)
-  console.log(STATE)
-}
+  $(".start-screen").attr("hidden", true);
+  $(".perfect-tweet-screen").attr("hidden", false);
+};
 
 const listenForSearchButtonClick = () => {
-  console.log('listenForSearchButtonClick')
-  $('.start-screen').on('click', 'button', function (event) {
-    event.preventDefault()
-    searchButtonClick(this)
-  })
-}
+  console.log("listenForSearchButtonClick");
+  $(".start-screen").on("click", "button", function(event) {
+    event.preventDefault();
+    searchButtonClick(this);
+  });
+};
 
 const switchToStartScreen = () => {
-  STATE.currentScreen = 'start'
-  $('.start-screen').attr('hidden', false)
-  $('.perfect-tweet-screen').attr('hidden', true)
-}
+  $(".start-screen").attr("hidden", false);
+  $(".perfect-tweet-screen").attr("hidden", true);
+};
 
 const resetInfo = () => {
   STATE.info = {
-    wiki: { title: '', extract: '' },
+    wiki: { title: "", extract: "" },
     giphy: [],
     news: {
       good: [],
       everything: []
-    },
-  }
-}
+    }
+  };
+};
 
 const restartButtonClick = () => {
-  console.log('restart click')
-  STATE.topic = ''
-  resetInfo()
-  switchToStartScreen()
-}
+  console.log("restart click");
+  STATE.topic = "";
+  resetInfo();
+  switchToStartScreen();
+};
 
 const listenForRestartButtonClick = () => {
-  console.log('listening for restart')
-  $('#start-over-button').click(function (event) {
-    restartButtonClick()
-  })
-}
+  console.log("listening for restart");
+  $("#start-over-button").click(function(event) {
+    restartButtonClick();
+  });
+};
 
 const wakeUpServer = () => {
-  fetch(TWITTER_SEARCH_URL + 'wakeUp/', {
-    method: 'get',
-    mode: 'cors',
+  fetch(TWITTER_SEARCH_URL + "wakeUp/", {
+    method: "get",
+    mode: "cors"
   })
     // .then(res => res.json())
-    .then(text => console.log('Poking the bear -> ', text))
-}
+    .then(text => console.log("Poking the bear -> ", text));
+};
 
 const handlePerfectTweetApp = () => {
-  listenForSearchButtonClick()
-  listenForRestartButtonClick()
+  listenForSearchButtonClick();
+  listenForRestartButtonClick();
   // listenForTweetButtonClick()
   //Need to make this!!
-  wakeUpServer()
-}
+  wakeUpServer();
+};
 
-$(handlePerfectTweetApp)
+$(handlePerfectTweetApp);
